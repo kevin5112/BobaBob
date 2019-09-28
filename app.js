c78979f57e039a8145c18485c518673fd7379e08
RJ Esquivias <esquiviasrj@gmail.com>
2019-09-27 17:13:05 -0700

    Add basic webpage serving

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.set("view engine", "ejs");
 
 var port = 3000;
 
+app.get("/", function(req, res) {
+	res.render("index");
+})
+
 app.listen(port, function(){
    console.log("listening on port: " + port + "...");
 })
