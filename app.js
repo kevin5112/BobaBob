f60d0ec91e55eadf9ea455ec7662715306c63066
RJ Esquivias <esquiviasrj@gmail.com>
2019-09-28 19:07:58 -0700

    Make Shop object and pass to ejs file

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.get("/", function(req, res) {
 })
 
 app.get("/shops", function(req, res){
-   res.render("shops")
+   var shops = [
+      {name: "Sharetea", image: "Sharetea_logo.png", description: "500 teahouses in 15 countries that import their authentic tea leaves from Taiwan. Stop by a location today and sip share love!"},
+      {name: "Kung Fu Tea", image: "KungFuTea_logo.png", description: "Welcome to Kung Fu Tea's Fresh - Innovative - Fearless world of bubble tea (boba) and other tasty, refreshing beverages."}
+   ]
+   res.render("shops", {shops:shops})
 })
 
 app.listen(port, function(){
