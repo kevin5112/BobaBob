557626fe36bd03172a68d9280c3b52b79d8fc418
RJ Esquivias <esquiviasrj@gmail.com>
2019-09-28 18:50:26 -0700

    Add gitignore

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require("express")
-var app = express();
+var app = express()
 
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
 
 var port = 3000;
 
 app.get("/", function(req, res) {
-	res.render("index");
+	res.render("index")
 })
 
 app.get("/display", function(req, res){
